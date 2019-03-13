69fd1c4069b5ecf915dcf28b6d083abaff998c31
klianeva <vikiklianeva@gmail.com>
2019-03-13 16:35:56 +0100

    adds also the hex name

--- color_check/scripts/check.py
+++ color_check/scripts/check.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import cgi
 import csv
-import math
 
 form = cgi.FieldStorage()
 color_input = form.getvalue('color').title()
@@ -10,8 +9,10 @@ color_input = form.getvalue('color').title()
 with open('scripts/colornames.csv', mode="r") as csv_file:
 	csv_reader = csv.DictReader(csv_file)
 	exist = False
+	hexx = None
 	for row in csv_reader:
-		if color_input == row["name"] or color_input == row["hex"]: 
+		if color_input == row["name"] or color_input == row["hex"]:
+			hexx = row["hex"]
 			exist = True
 
 	if exist == True:
@@ -19,11 +20,11 @@ with open('scripts/colornames.csv', mode="r") as csv_file:
 			<html>
 			<body>
 			<p>
-				%s is a real color
+				%s is a real color and its's hex is: %s
 			</p>
 			</body>
 			</html>
-			""" % color_input)
+			""" % (color_input, hexx))
 	else: 
 		print("""
 			<html>
--- color_check/scripts/smth.py
+++ color_check/scripts/smth.py
@@ -4,7 +4,5 @@ color_input = "Yellow"
 with open('colornames.csv', mode="r") as csv_file:
 	csv_reader = csv.DictReader(csv_file)
 	for row in csv_reader:
-		if color_input == row["name"]: 
-			print("True")
-
-	
+		if color_input == row["name"] or color_input == row["hex"]: 
+			print(row["hex"])
