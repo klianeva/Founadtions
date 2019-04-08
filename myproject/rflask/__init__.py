8ef167e5719098cac19b2d9df0b88366a7b4de4b
klianeva <vikiklianeva@gmail.com>
2019-04-08 16:45:53 +0200

    my project

---
+++ myproject/rflask/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template, session 
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hellow():
+	return "Hello"
+
+def factors(num):
+	return [x for x in range (1, num+1) if num%x==0]
+
+@app.route("/factors/<int:n>")
+def factors_route(n):
+	return render_template(
+		"factors.html",  # name of template
+		number=n,  # value for `number` in template
+		factors=factors(n) # value for `factors` in template
+	)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
