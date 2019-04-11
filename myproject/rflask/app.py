c57218e77bb859fe0cce3930983deb93edef5a6b
klianeva <vikiklianeva@gmail.com>
2019-04-11 13:53:13 +0200

    last updates on the database

--- myproject/rflask/app.py
+++ myproject/rflask/app.py
@@ -1,25 +1,54 @@
 import os
-from flask import Flask, render_template, session 
-
+from flask import Flask, render_template, session, request
+import sqlite3 as sql
 
 app = Flask(__name__)
 
 @app.route("/")
-def hellow():
-	return "Hello"
-
-def factors(num):
-	return [x for x in range (1, num+1) if num%x==0]
-
-@app.route("/factors/<int:n>")
-def factors_route(n):
-	return render_template(
-		"factors.html",  # name of template
-		number=n,  # value for `number` in template
-		factors=factors(n) # value for `factors` in template
-	)
+def hello():
+    return render_template("index.html")
 
 
+@app.route('/enternew')
+def new_story():
+   return render_template('stories_temp.html')
+
+@app.route("/addstr", methods = ['POST', 'GET'])
+def addstr():
+    if request.method == 'POST':
+        try:
+            FirstName = request.form['FirstName']
+            LastName = request.form['LastName']
+            Story = request.form['Story']
+        
+            with sql.connect("stories.db") as con:
+                cur = con.cursor()
+
+            # sql_statement_one = """INSERT INTO women(FirstName,LastName) 
+            #   VALUES (?,?)""";
+            # sql_statement_two = """INSERT INTO stories(Story) 
+            #   VALUES (?)""";
+
+            #PROBLEM: How to insert data from one form into 2 tables?
+
+             # variable = cur.execute(""" JOIN stories ON Women.WomenID=stories.StrWomenID""")
+                # var_new = variable[-1]
+                # cur.execute("""INSERT INTO stories (Story,StrWomenID) 
+                #     VALUES (?) AND JOIN stories ON Women.WomenID=stories.StrWomenID""", (Story))
+                # con.commit()   
+                          
+                # 
+                
+                cur.execute(""" INSERT INTO Women (FirstName, LastName,Story) 
+                    VALUES (?,?,?)""", (FirstName, LastName,Story))
+                con.commit()                
+                msg = "Your story has successfully been added"
+        except:
+            con.rollback()
+            msg = "error damn it"
+    return render_template("done.html", msg= msg)
+    con.close()
 
 if __name__ == "__main__":
-    app.run(debug=True,port = 80, address="0.0.0.0")
+    app.run(debug=True)
+#,port = 80, address="0.0.0.0"
--- myproject/rflask/db.py
+++ myproject/rflask/db.py
@@ -3,3 +3,8 @@ import sqlite3
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
+
+conn = sqlite3.connect('stories.db')
+print "Opened database successfully";
+
+conn.close()
